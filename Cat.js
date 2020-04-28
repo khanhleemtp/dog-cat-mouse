405b52b957233b256eecba4627f33d820aee2546
LdKhanh <khanhleemtp>
2020-04-29 03:23:32 +0700

    Modified Cat.js

--- Cat.js
+++ Cat.js
@@ -1,3 +1,24 @@
-function Cat(name) {
+function Cat() {
+	this.stomath = [];
+};
+
+function Mouse(name) {
 	this.name = name;
-}
+	this.isLive = true; 
+};
+
+
+
+Cat.prototype.eat = function(mouse) {
+	this.stomath.push(mouse);
+	mouse.isLive = false;
+};
+
+var tom = new Cat();
+
+var mickey = new Mouse('mickey');
+
+tom.eat(mickey);
+
+console.log(tom);
+
